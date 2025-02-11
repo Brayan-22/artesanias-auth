a5ca4d6611b8dca3f959c56d4a56adab2ce04564
alejandro <alejandroriveros100@gmail.com>
2025-02-11 04:15:03 -0500

    xd

--- src/main/java/com/artesanias/authenticationservice/AuthenticationServiceApplication.java
+++ src/main/java/com/artesanias/authenticationservice/AuthenticationServiceApplication.java
@@ -11,7 +11,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class AuthenticationServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AuthenticationServiceApplication.class, args);
     }
